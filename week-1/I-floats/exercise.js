b937885dd0645998d77a138f91d328617d79ee30
Mykola Marenich <marenich.mykola@gmail.com>
2020-02-13 21:44:48 +0100

    week1

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,9 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var total = numberOfMentors + numberOfStudents;
+var percStud = Math.round((100 * numberOfStudents) / total);
+var percMent = Math.round((100 * numberOfMentors) / total);
+
+
+console.log(percStud);
+console.log(percMent);
